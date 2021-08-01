28605d304afffeba6e06bcef99cf97abefda97c0
realaravinth <realaravinth@batsense.net>
2021-08-01 09:38:21 +0000

    Deploying to gh-pages from  @ ce3bd79e59b657c1b388fd2920b409c6fccf4576 🚀

--- cli/sidebar-items.js
+++ cli/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"constant":[["VERSION",""]],"enum":[["Mode",""]],"mod":[["commands",""],["errors",""],["handlers",""],["options",""]],"struct":[["State",""]]});
+initSidebarItems({"constant":[["VERSION",""]],"enum":[["Mode","Different “modes” in which the applications can operate at"]],"mod":[["commands",""],["errors",""],["handlers",""],["options",""]],"struct":[["State","Application state"]]});
